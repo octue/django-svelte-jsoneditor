6adf5ba00618c6624332a6755a3c8f308b996c98
Graham Hoyes <accounts@grahamhoyes.com>
2024-02-26 09:29:38 -0500

    ENH: Allow setting the container class

--- django_svelte_jsoneditor/widgets.py
+++ django_svelte_jsoneditor/widgets.py
@@ -7,11 +7,13 @@ from .settings import check_props, get_props
 class SvelteJSONEditorWidget(Textarea):
     template_name = "django_svelte_jsoneditor/widgets/svelte_jsoneditor.html"
 
-    def __init__(self, props=None, attrs=None):
+    def __init__(self, props=None, attrs=None, wrapper_class="svelte-jsoneditor-wrapper"):
         if attrs is None:
             attrs = {}
 
         self.props = {} if props is None else props.copy()
+        self.wrapper_class = wrapper_class
+
         check_props(self.props)
         attrs.update({"class": "hidden"})
 
@@ -20,6 +22,7 @@ class SvelteJSONEditorWidget(Textarea):
     def get_context(self, name, value, attrs):
         context = super().get_context(name, value, attrs)
         context["widget"].update({"props": json.dumps({**get_props(), **self.props})})
+        context["widget"].update({"wrapper_class": self.wrapper_class})
         return context
 
     class Media:
